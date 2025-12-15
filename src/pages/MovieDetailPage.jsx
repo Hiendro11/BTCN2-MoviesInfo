95a68c8a0cb3794677bb6788deda980a1eb3c5d1
22120102 <txm.hien1102@gmail.com>
2025-12-16 05:22:48 +0700

    feat: hiển thị danh sách đạo diễn và diễn viên chính kèm link

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/MovieDetailPage.jsx
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { getMovieDetail, getMovieReviews } from '../api/movies'
